93dd3faee66b0740ff291809800ef4f9e8e88275
後藤尋 <100586810+hir05o1@users.noreply.github.com>
2025-06-15 15:29:51 +0900

    Add-a-control-to-toggle-the-state

---
+++ creating-conbining-view/Views/Helper/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  creating-conbining-view
+//
+//  Created by hiro on 2025/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
