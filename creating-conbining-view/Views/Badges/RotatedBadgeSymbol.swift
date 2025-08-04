73520ca8676142dde0d8dcaa4c3be1ae91b85b12
hir05o1 <100586810+hir05o1@users.noreply.github.com>
2025-08-04 20:53:45 +0900

    add: Badge

---
+++ creating-conbining-view/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  creating-conbining-view
+//
+//  Created by hiro on 2025/08/04.
+//
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
